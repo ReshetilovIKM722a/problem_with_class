9eea77421c15fc9663acb35560b0b5ee449bc2a2
FrogHus <FrogHus@DESKTOP-G345A7B>
2024-03-26 09:29:43 +0100

    Add project files.

---
+++ problem_with_class/Internet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_with_class
+{
+    internal class Internet
+    {
+        private double speed;
+        private string name;
+        private double cost;
+        private string technology;
+
+        public Internet(double speed, string name, double cost, string technology)
+        {
+            this.speed = speed;
+            this.name = name;
+            this.cost = cost;
+            this.technology = technology;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Internet speed: {speed} Mb/s");
+            Console.WriteLine($"Provider's name: {name}");
+            Console.WriteLine($"Internet cost: {cost} UAH per month");
+            Console.WriteLine($"Internet technology: {technology}");
+        }
+
+        public void UpdateSpeed(double speed)
+        {
+            this.speed = speed;
+            Console.WriteLine("\n Speed was noted");
+        }
+
+        public bool NeedsService()
+        {
+            return speed < 100; 
+        }
+
+        public void ResetSpeed()
+        {
+            Console.WriteLine("\n Master do his job...");
+            speed = 100;
+            Console.WriteLine("Internet is normal now");
+        }
+
+    }
+}
---
+++ problem_with_class/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Створити клас. Описати поля та методи класу. Надіслати гітхаб репозиторій із створеним класом у гул клас.
+Інтернет-провайдер
+*/
+using problem_with_class;
+
+var Internet = new Internet(100, "Triolan", 240, "Ethernet");
+
+Internet.DisplayInfo();
+
+Internet.UpdateSpeed(100);
+
+if (Internet.NeedsService())
+{
+    Console.WriteLine("You need to call the master");
+    Internet.ResetSpeed();
+}
+else
+{
+    Console.WriteLine("Everything is fine");
+}
+
+Internet.UpdateSpeed(80);
+
+if (Internet.NeedsService())
+{
+    Console.WriteLine("You need to call the master");
+    Internet.ResetSpeed();
+}
+else
+{
+    Console.WriteLine("Everything is fine");
+}
